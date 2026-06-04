77e14e0fcc789e2edceeaa339bee290edbd9311f
unknown <herbertgaleano2020@gmail.com>
2026-06-04 12:53:52 -0600

    Cambios iniciales en API - Creación de modelos base (Models/Entities/) AntenasTerrestres, Mensajes, Orbita, Satelite y Simulacion de Estado

---
+++ Orbinet.Web/Models/Entities/GroundAntenna.cs
@@ -0,0 +1,7 @@
+public class GroundAntenna{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Coords { get; set; }
+    public string Ip {get; set; }
+} 
+
