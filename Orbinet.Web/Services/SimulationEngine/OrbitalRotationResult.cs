ec0ab6ffbdfbc9f2fbaf0399e217d49cd0e5cd44
Rudin López <1907858020101@ingenieria.usac.edu.gt>
2026-06-26 08:07:18 -0600

    Agregue el archivo OrbitalRotationResult.cs dentre de la carpeta Services\SimulationEngine\ para que OrbitalRotator.cs funcione bien

---
+++ Orbinet.Web/Services/SimulationEngine/OrbitalRotationResult.cs
@@ -0,0 +1,16 @@
+namespace Orbinet.Web.Services.SimulationEngine
+{
+    public class OrbitalRotationResult
+    {
+        public int SatellitesDetected { get; set; }
+        public int RotatedSuccessfully { get; set; }
+        public int SkippedByCollision { get; set; }
+        public int UnchangedPositions { get; set; }
+        public string Details { get; set; }
+
+        public OrbitalRotationResult()
+        {
+            Details = string.Empty;
+        }
+    }
+}
